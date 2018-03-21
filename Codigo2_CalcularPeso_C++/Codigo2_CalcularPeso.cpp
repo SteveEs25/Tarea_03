25dec2f83649762d5dfc1240b22758cc9c99543f
SteveEs25 <nalfaro090@gmail.com>
2018-03-20 23:34:36 -0600

    Finalizando Proyecto 2

--- Codigo2_CalcularPeso_C++/Codigo2_CalcularPeso.cpp
+++ Codigo2_CalcularPeso_C++/Codigo2_CalcularPeso.cpp
@@ -7,8 +7,9 @@ int main()
 	double fuerza;
 	double peso;
 	
-	cout<<"Ingrese la fuerza:\n";
+	cout<<"Ingrese la fuerza: ";
 	cin>>fuerza;
+	cout<<"Gravedad constante de 9.8 m/s^2\n\n";
 	
 	peso = fuerza/9.8;
 	cout<<"El Peso es de: "<<peso<<" Kg\n";
