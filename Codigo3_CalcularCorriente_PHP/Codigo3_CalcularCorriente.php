b1fbaf4c51bb342bbe4ab1e1d3689c0f487fd590
SteveEs25 <nalfaro090@gmail.com>
2018-03-21 01:41:09 -0600

    Finalizando Proyecto 3
    Terminando Proyecto de Corriente

--- Codigo3_CalcularCorriente_PHP/Codigo3_CalcularCorriente.php
+++ Codigo3_CalcularCorriente_PHP/Codigo3_CalcularCorriente.php
@@ -5,15 +5,30 @@
 </head>
 <body>
 
-	<form method="post" action="">
+	<h1>Calcular Corriente</h1>
+
+	<form method="post">
 		<label>Ingrese el Voltaje:</label> <br>
 		<input type="text" name="voltaje"> <br><br>
 
 		<label>Ingrese la Resistencia:</label> <br>
 		<input type="text" name="resistencia"> <br><br>
 
-		<input type="submit" value="corriente">
+		<input type="submit" value="Calcular">
 	</form>
+	<br>
 
 </body>
+
+<?php
+	if(isset($_POST['voltaje']) && isset($_POST['resistencia']))
+	{
+		$volt = $_POST['voltaje'];
+		$resist = $_POST['resistencia'];
+
+		$corriente = $volt / $resist;
+		echo "La Corriente tiene una intensidad de: ".$corriente." Amperios";
+	}
+?>
+
 </html>
