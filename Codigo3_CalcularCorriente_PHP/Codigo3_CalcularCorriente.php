d51a5da1c87f1524e684ca768803e8787528336e
SteveEs25 <nalfaro090@gmail.com>
2018-03-21 00:35:51 -0600

    Creando Proyecto 3
    Creando Proyecto de Corriento con Lenguaje PHP

---
+++ Codigo3_CalcularCorriente_PHP/Codigo3_CalcularCorriente.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calcular Corriente</title>
+</head>
+<body>
+
+	<form method="post" action="">
+		<label>Ingrese el Voltaje:</label> <br>
+		<input type="text" name="voltaje"> <br><br>
+
+		<label>Ingrese la Resistencia:</label> <br>
+		<input type="text" name="resistencia"> <br><br>
+
+		<input type="submit" value="corriente">
+	</form>
+
+</body>
+</html>
